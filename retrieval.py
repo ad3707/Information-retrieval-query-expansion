d58272dbf41d011d89dfb6312287b3bc69ef02fb
ad3707 <93938930+ad3707@users.noreply.github.com>
2022-02-13 14:51:27 -0500

    Update retrieval.py

--- retrieval.py
+++ retrieval.py
@@ -1,1 +1,16 @@
+import pprint
+from googleapiclient.discovery import build
 
+
+def main():
+  service = build("customsearch", "v1",
+            developerKey="AIzaSyDRRpR3GS1F1_jKNNM9HCNd2wJQyPG3oN0")
+
+  res = service.cse().list(
+      q='lectures',
+      cx='017576662512468239146:omuauf_lfve',
+    ).execute()
+  pprint.pprint(res)
+
+if __name__ == '__main__':
+  main()
